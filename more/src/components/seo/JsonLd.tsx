618033cd0692ea6b9c6a0005cf652c0c2d070107
yoimdely <yoimdely@gmail.com>
2026-02-19 16:34:44 +0300

    Delete more directory

--- more/src/components/seo/JsonLd.tsx
+++
@@ -1,8 +0,0 @@
-export default function JsonLd({ data }: { data: any }) {
-  return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-    />
-  );
-}
